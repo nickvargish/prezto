9a2accf618dcfb82c89c6db09cb1a0001a0784c0
Nick Vargish <nvargish@cisco.com>
2015-05-11 08:25:31 -0400

    Sublimetext more friendly to non OSX.

--- modules/sublimetext/README.md
+++ modules/sublimetext/README.md
@@ -2,9 +2,14 @@ Sublimetext Shell integration
 ==============================
 
 Provides some useful aliases
-Adjust the subl alias for your OS/sublimetext version.  This assumes sublimetext3 for osx
 
+Adjust the subl alias for your OS/sublimetext version.
 
+Looks for 'subl' on $path, also checks
+  - /Applications/Sublime\ Text.app
+
+Aliases
+-------
 
 - `subl <file or directory>` edit File / open directory with SublimeText
 - `subla <file or directory` load the specified file or directory into the current sublimetext window
--- modules/sublimetext/init.zsh
+++ modules/sublimetext/init.zsh
@@ -1,9 +1,21 @@
+
+_sublbin=$(whence subl)
+if [[ -z "$_sublbin" ]]; then
+	if [[ -x '/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl' ]]; then
+		_sublbin='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
+	fi
+fi
+if [[ -z "$_sublbin" ]]; then
+	return 1
+fi
+
 # Open the specified file and/or directory in a new window
-alias subl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl -n'
+alias subl="$_sublbin -n"
 
 # Add the specified file and/or directory to the current winsow
-alias subla='subl -a'
-
+alias subla='$_sublbin -a'
 
 # load a project
-alias sublp="subl --project"
+alias sublp="$_sublbin --project"
+
+return 0
