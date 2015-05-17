d041f2f28771ae663cdbbd3bd69160b599ecfe38
Nick Vargish <nick@vargish.org>
2015-05-17 10:21:16 -0400

    commented out -i alias for cp and mv.

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -48,11 +48,11 @@ alias sftp='noglob sftp'
 # Define general aliases.
 alias _='sudo'
 alias b='${(z)BROWSER}'
-alias cp="${aliases[cp]:-cp} -i"
+#alias cp="${aliases[cp]:-cp} -i"
 alias e='${(z)VISUAL:-${(z)EDITOR}}'
 alias ln="${aliases[ln]:-ln} -i"
 alias mkdir="${aliases[mkdir]:-mkdir} -p"
-alias mv="${aliases[mv]:-mv} -i"
+#alias mv="${aliases[mv]:-mv} -i"
 alias p='${(z)PAGER}'
 alias po='popd'
 alias pu='pushd'
